13422c52550b91a484ad1ad60ad6af14ff535ffa
vancode8x <vanthq@gmail.com>
2025-04-24 08:25:40 +0700

    change config to new folder path

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-    'base' => '/PTIT24CNTT01/ss21/',
-    'assets' => '/PTIT24CNTT01/ss21/assets/',
-    'baseURL' => 'http://localhost:88/PTIT24CNTT01/ss21/', 
+    'base' => '/shopmvc/',
+    'assets' => '/shopmvc/assets/',
+    'baseURL' => 'http://localhost:88/shopmvc/', 
     'db' => array(
         'host'     => 'localhost',
         'name'     => 'ProductDB',
